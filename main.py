b08a0789bca2e1a1a5f1987e3a1e7a7c490cd540
ddkktt <128318872+ddkktt@users.noreply.github.com>
2024-02-03 19:06:39 -0600

    a

--- main.py
+++ main.py
@@ -1,21 +1,18 @@
-import uuid
 
+import uuid
 
 class Movie():
     '''class for movies'''
-    def __init__(self,title:str, price: int, description: str) -> None:
+    def __init__(self,title:str, price: int, description: str, imgURL:str) -> None:
         '''initializes a movie'''
         self.title:str = title
         self.price:int = price
         self.description:str = description
         self.rentals:int = 0
+        self.imgURL:str = imgURL
+    def __str__(self) -> str:
+        return f'\n{self.title}\n{self.price}\n{self.rentals}\n'
     
-
-
-
-    
-
-
 class User():
     '''class for a user'''
     def __init__(self, username:str, password:str, email:str) -> None:
@@ -25,13 +22,10 @@ class User():
         self.email:str = email
         self.accounts:[Account] = []
 
-    def add_account(self,name:str,balance:int) -> None:
+    def add_account(self,name:str,balance:int):
         '''adds and links an account to a user'''
         a = Account(name, balance)
-        
         self.accounts.append(a)
-        
-
     
 class Account(User):
     '''class for account'''
@@ -45,7 +39,7 @@ class Account(User):
         '''print out a users name, balance and rented movies'''
         return f'name: {self.name}, balance {self.balance}, movies {self.rented_movies}'
     
-    def rent_movies(self, movie:Movie):
+    def rent_movie(self, movie:Movie):
         '''method to rent a movie'''
         if(self.balance>movie.price):
             self.balance -= movie.price
@@ -54,15 +48,11 @@ class Account(User):
         
 
 
-
-
-class Movie():
-    '''class for movies'''
-
-
-
 u = User("david",'iteso', 'dtks')
 
-u.add_account('netflix', 5)
+a = u.add_account('netflix', 5)
 
-print(u.accounts[0])
+m = Movie("toy story",3,'movie about toy story', 'google.com')
+
+u.accounts[0].rent_movie(m)
+
