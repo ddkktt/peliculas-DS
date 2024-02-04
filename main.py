8500995276ecb306a020cb34d999ed805c6c12ec
ddkktt <128318872+ddkktt@users.noreply.github.com>
2024-02-03 18:56:21 -0600

    rent

--- main.py
+++ main.py
@@ -1,31 +1,56 @@
 import uuid
 
 
-class Account():
-    '''class for account'''
-    def __init__(self, name:str, balance:int) -> None:
-        self.name = name
-        self.balance = balance
-        self.rented_movies = []
-    def __str__(self) -> str:
-        return f'name: {self.name}, balance {self.balance}, movies {self.rented_movies}'
-    def rent_movies():
-        pass
+class Movie():
+    '''class for movies'''
+    def __init__(self,title:str, price: int, description: str) -> None:
+        '''initializes a movie'''
+        self.title:str = title
+        self.price:int = price
+        self.description:str = description
+        self.rentals:int = 0
+    
+
+
+
     
 
 
 class User():
     '''class for a user'''
-    def __init__(self,username:str, password:str, email:str) -> None:
+    def __init__(self, username:str, password:str, email:str) -> None:
         self.username:str = username
         self.userID:str = uuid.uuid4()
         self.password:str = password
         self.email:str = email
         self.accounts:[Account] = []
-    def add_account(self,name:str,balance:int):
+
+    def add_account(self,name:str,balance:int) -> None:
+        '''adds and links an account to a user'''
         a = Account(name, balance)
+        
         self.accounts.append(a)
+        
+
     
+class Account(User):
+    '''class for account'''
+    def __init__(self, name:str, balance:int) -> None:
+        '''initializes an account'''
+        self.name:str = name
+        self.balance:int = balance
+        self.rented_movies:[Movie] = []
+
+    def __str__(self) -> str:
+        '''print out a users name, balance and rented movies'''
+        return f'name: {self.name}, balance {self.balance}, movies {self.rented_movies}'
+    
+    def rent_movies(self, movie:Movie):
+        '''method to rent a movie'''
+        if(self.balance>movie.price):
+            self.balance -= movie.price
+            movie.rentals += 1
+            self.rented_movies.append(movie)
         
 
 
